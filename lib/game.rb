7be6fa37736ec45f5b3f33fbe82256962cba5284
Jakub Cembala <cembala.jakub@gmail.com>
2021-08-08 08:57:18 +0200

    Game class template, welcome message
    - added welcome message
    - asking player for input
    - reading guess from player
    - moved word initialization to Game class
    - added setup method

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,39 @@
 # frozen_string_literal: true
 
+require_relative 'words'
+
 # Manages game: starting, looping, ending
 class Game
+  def initialize
+    @word = ''
+    @guess = ''
+  end
 
+  def start
+    setup_game
+    show_welcome_message
+    ask_for_guess
+    @guess
+  end
+
+  private
+
+  def setup_game
+    words = Words.new('wordslist.txt')
+    @word = words.randomize_word
+  end
+
+  def show_welcome_message
+    puts '----------------------------------------------------------------'
+    puts 'Welcome to Hangman'
+    puts 'We randomize one 5 to 12 character word and you have to guess it'
+    puts 'If you can guess the word before you will be hanged. You win.'
+    puts 'Otherwise, you lose.'
+    puts '----------------------------------------------------------------'
+  end
+
+  def ask_for_guess
+    print 'Your guess letter is: '
+    @guess = gets.chomp
+  end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative 'words'
+require_relative 'game'
 
-words = Words.new('wordslist.txt')
-words.randomize_word
+game = Game.new
+p game.start
